cd94738911ab97e4d94431b19389ea5c9f597682
Roi Dopazo <roidopazo@gmail.com>
2022-01-08 19:39:43 +0100

    initial commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import GameScreen from 'screens/game/GameScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <GameScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
